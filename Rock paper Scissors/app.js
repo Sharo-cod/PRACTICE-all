b75b6cddc49abe4d7ad212cd614992796ce08305
Shariq Alam <sharo4192@gmail.com>
2024-10-19 14:33:38 +0500

    new

--- Rock paper Scissors/app.js
+++ Rock paper Scissors/app.js
@@ -2,9 +2,19 @@ let userScore = 0;
 let compScore = 0;
 const choices = document.querySelectorAll(".choice");
 
+const genCompChoice = () =>{
+    const option = ["Rock" ,"Paper" ,"Scissors"]
+}
+
+const playGame = () =>{
+    console.log("userChoice =", userChoice);
+    
+}
+
 choices.forEach((choice) => {
     console.log(choice);
     choice.addEventListener("click" , () => {
-        console.log("choices was clicked");
+        const userChoice = choice.getAttribute("id");
+        playGame(userChoice);
     });
 });
