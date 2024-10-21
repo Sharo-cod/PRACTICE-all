76776884b70c7d03ceff8036e321cc0d8d3ca4ab
Shariq Alam <sharo4192@gmail.com>
2024-10-21 14:55:45 +0500

    new js

--- Rock paper Scissors/app.js
+++ Rock paper Scissors/app.js
@@ -3,16 +3,27 @@ let compScore = 0;
 const choices = document.querySelectorAll(".choice");
 
 const genCompChoice = () =>{
-    const option = ["Rock" ,"Paper" ,"Scissors"]
+    const options = ["Rock" ,"Paper" ,"Scissors"]
+    const randomIdx = Math.floor(Math.random() *3);
+    return options[randomIdx]; 
+}
+const drawGame = () => {
+    console.log("GAME WAS DRAW");
 }
 
-const playGame = () =>{
+
+const playGame = (userChoice) =>{
     console.log("userChoice =", userChoice);
-    
-}
+    const compChoice = genCompChoice();
+    console.log("Comp Choice =", compChoice);
+
+    if( userChoice === compChoice){
+        drawGame();
+    }
+};
+
 
 choices.forEach((choice) => {
-    console.log(choice);
     choice.addEventListener("click" , () => {
         const userChoice = choice.getAttribute("id");
         playGame(userChoice);
